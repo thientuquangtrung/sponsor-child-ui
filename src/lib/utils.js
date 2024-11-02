140a58b8f45b33b41b5d0f0fd9c5499f8eebe29a
Trung Nguyen <trung@intra.co.kr>
2024-11-02 11:26:06 +0700

    Fix: get location base on province || district

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,5 +1,5 @@
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs) {
     return twMerge(clsx(inputs));
@@ -19,12 +19,12 @@ export function humanFileSize(bytes, si = true, dp = 1) {
     const thresh = si ? 1000 : 1024;
 
     if (Math.abs(bytes) < thresh) {
-        return bytes + " B";
+        return bytes + ' B';
     }
 
     const units = si
-        ? ["kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
-        : ["KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"];
+        ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+        : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
     let u = -1;
     const r = 10 ** dp;
 
@@ -33,5 +33,14 @@ export function humanFileSize(bytes, si = true, dp = 1) {
         ++u;
     } while (Math.round(Math.abs(bytes) * r) / r >= thresh && u < units.length - 1);
 
-    return bytes.toFixed(dp) + " " + units[u];
+    return bytes.toFixed(dp) + ' ' + units[u];
 }
+
+/**
+ * Format number to string with comma separator
+ * @param {number} value
+ * @returns {string}
+ */
+export const formatNumber = (value) => {
+    return value.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+};
