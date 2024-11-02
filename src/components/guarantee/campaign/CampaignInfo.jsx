844f767bed82edcc642acf1e5dd932d720aaebb4
Nguyễn Trịnh Quang Trung <thientuquangtrung@gmail.com>
2024-11-02 11:27:28 +0700

    Trungntq (#71)
    * Fix: get location base on province || district
    
    ---------
    
    Co-authored-by: Trung Nguyen <trung@intra.co.kr>

--- src/components/guarantee/campaign/CampaignInfo.jsx
+++ src/components/guarantee/campaign/CampaignInfo.jsx
@@ -1,47 +1,55 @@
 import React, { useState, useCallback, useEffect } from 'react';
 import { useForm, useFieldArray } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Button } from "@/components/ui/button";
-import { Form } from "@/components/ui/form";
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { Loader2 } from 'lucide-react';
 import { toast } from 'sonner';
 import { useSelector } from 'react-redux';
-import { useCreateCampaignMutation } from '@/redux/campaign/campaignApi';
 import { useNavigate } from 'react-router-dom';
+
+import { Button } from '@/components/ui/button';
+import { Form } from '@/components/ui/form';
+import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
+import { useCreateCampaignMutation } from '@/redux/campaign/campaignApi';
 import useLocationVN from '@/hooks/useLocationVN';
 import ChildInfo from '@/components/guarantee/campaign/ChildInfo';
 import DisbursementInfo from '@/components/guarantee/campaign/DisbursementInfo';
 import CampaignDetailInfo from '@/components/guarantee/campaign/CampaignDetailInfo';
 import addCampaignSchema from '@/components/schema/addCampaignSchema';
+import { formatNumber } from '@/lib/utils';
 
-const CampaignInfo = ({ }) => {
-    const [imagesFolderUrl, setImagesFolderUrl] = useState([]);
+const CampaignInfo = () => {
     const navigate = useNavigate();
+    const { user } = useSelector((state) => state.auth);
+
+    const [imagesFolderUrl, setImagesFolderUrl] = useState([]);
     const [childFile, setChildFile] = useState(null);
     const [thumbnail, setThumbnail] = useState(null);
-    const { user } = useSelector((state) => state.auth);
-    const [createCampaign, { isLoading: isCreatingCampaign }] = useCreateCampaignMutation();
     const [isUploading, setIsUploading] = useState(false);
+
+    const [createCampaign, { isLoading: isCreatingCampaign }] = useCreateCampaignMutation();
     const {
         provinces,
         districts,
         wards,
+        selectedProvince,
+        selectedDistrict,
+        selectedWard,
         setSelectedProvince,
         setSelectedDistrict,
         setSelectedWard,
     } = useLocationVN();
+
     const form = useForm({
         resolver: zodResolver(addCampaignSchema),
         defaultValues: {
-            childName: "",
+            childName: '',
             childBirthYear: new Date().getFullYear() - 1,
             childGender: 0,
-            childLocation: "",
+            childLocation: '',
             childIdentificationInformationFile: null,
-            provinceId: "",
-            districtId: "",
-            wardId: "",
+            provinceId: '',
+            districtId: '',
+            wardId: '',
             title: '',
             story: '',
             targetAmount: '',
@@ -56,54 +64,27 @@ const CampaignInfo = ({ }) => {
                 {
                     disbursementAmount: 0,
                     scheduledDate: new Date(),
-                    description: ''
+                    description: '',
                 },
                 {
                     disbursementAmount: 0,
                     scheduledDate: new Date(new Date().setMonth(new Date().getMonth() + 1)),
-                    description: ''
-                }
-            ]
+                    description: '',
+                },
+            ],
         },
-        mode: "onChange",
+        mode: 'onChange',
     });
-    const campaignType = form.watch("campaignType");
+
+    const campaignType = form.watch('campaignType');
     const { fields, append, remove } = useFieldArray({
         control: form.control,
-        name: "disbursementStages",
+        name: 'disbursementStages',
         rules: {
-            minLength: campaignType === 0 ? 2 : 1
-        }
+            minLength: campaignType === 0 ? 2 : 1,
+        },
     });
-    const handleProvinceChange = (provinceId) => {
-        setSelectedProvince(provinces.find(p => p.id === provinceId));
-        form.setValue('provinceId', provinceId);
-        form.setValue('districtId', '');
-        form.setValue('wardId', '');
-        form.trigger('provinceId');
-    };
 
-    const handleDistrictChange = (districtId) => {
-        setSelectedDistrict(districts.find(d => d.id === districtId));
-        form.setValue('districtId', districtId);
-        form.setValue('wardId', '');
-        form.trigger('districtId');
-    };
-
-    const handleWardChange = (wardId) => {
-        setSelectedWard(wards.find(w => w.id === wardId));
-        form.setValue('wardId', wardId);
-        form.trigger('wardId');
-    };
-
-
-    const handleRemove = (index) => {
-        if (campaignType === 0 && fields.length <= 2) {
-            toast.error('Chiến dịch nuôi em phải có ít nhất hai giai đoạn giải ngân');
-            return;
-        }
-        remove(index);
-    };
     useEffect(() => {
         const subscription = form.watch((value, { name }) => {
             if (name === 'campaignType') {
@@ -111,11 +92,13 @@ const CampaignInfo = ({ }) => {
                 const currentDate = new Date();
 
                 if (campaignType === 1) {
-                    form.setValue('disbursementStages', [{
-                        disbursementAmount: 0,
-                        scheduledDate: currentDate,
-                        description: ''
-                    }]);
+                    form.setValue('disbursementStages', [
+                        {
+                            disbursementAmount: 0,
+                            scheduledDate: currentDate,
+                            description: '',
+                        },
+                    ]);
                 } else {
                     const nextMonth = new Date(currentDate);
                     nextMonth.setMonth(currentDate.getMonth() + 1);
@@ -124,13 +107,13 @@ const CampaignInfo = ({ }) => {
                         {
                             disbursementAmount: 0,
                             scheduledDate: currentDate,
-                            description: ''
+                            description: '',
                         },
                         {
                             disbursementAmount: 0,
                             scheduledDate: nextMonth,
-                            description: ''
-                        }
+                            description: '',
+                        },
                     ]);
                 }
             }
@@ -153,25 +136,54 @@ const CampaignInfo = ({ }) => {
                     {
                         disbursementAmount: 0,
                         scheduledDate: currentDate,
-                        description: ''
+                        description: '',
                     },
                     {
                         disbursementAmount: 0,
                         scheduledDate: nextMonth,
-                        description: ''
-                    }
+                        description: '',
+                    },
                 ]);
             } else {
-                form.setValue('disbursementStages', [{
-                    disbursementAmount: 0,
-                    scheduledDate: currentDate,
-                    description: ''
-                }]);
+                form.setValue('disbursementStages', [
+                    {
+                        disbursementAmount: 0,
+                        scheduledDate: currentDate,
+                        description: '',
+                    },
+                ]);
             }
         }
     }, [form]);
 
+    const handleProvinceChange = (provinceId) => {
+        setSelectedProvince(provinces.find((p) => p.id === provinceId));
+        form.setValue('provinceId', provinceId);
+        form.setValue('districtId', '');
+        form.setValue('wardId', '');
+        form.trigger('provinceId');
+    };
 
+    const handleDistrictChange = (districtId) => {
+        setSelectedDistrict(districts.find((d) => d.id === districtId));
+        form.setValue('districtId', districtId);
+        form.setValue('wardId', '');
+        form.trigger('districtId');
+    };
+
+    const handleWardChange = (wardId) => {
+        setSelectedWard(wards.find((w) => w.id === wardId));
+        form.setValue('wardId', wardId);
+        form.trigger('wardId');
+    };
+
+    const handleRemove = (index) => {
+        if (campaignType === 0 && fields.length <= 2) {
+            toast.error('Chiến dịch nuôi em phải có ít nhất hai giai đoạn giải ngân');
+            return;
+        }
+        remove(index);
+    };
 
     const uploadToCloudinary = async (file, folder) => {
         const formData = new FormData();
@@ -186,7 +198,7 @@ const CampaignInfo = ({ }) => {
                 {
                     method: 'POST',
                     body: formData,
-                }
+                },
             );
 
             if (!response.ok) {
@@ -200,43 +212,55 @@ const CampaignInfo = ({ }) => {
             throw error;
         }
     };
-    const onDropChildFile = useCallback((acceptedFiles) => {
-        const file = acceptedFiles[0];
-        if (childFile) {
-            URL.revokeObjectURL(childFile.preview);
-        }
-        setChildFile(file);
-        form.setValue('childIdentificationInformationFile', file);
-        form.clearErrors('childIdentificationInformationFile');
-    }, [form, childFile]);
-    const onDropImagesFolder = useCallback((acceptedFiles) => {
-        const newImagesFolderUrl = acceptedFiles.map(file => Object.assign(file, {
-            preview: URL.createObjectURL(file)
-        }));
 
-        setImagesFolderUrl(prevImagesFolderUrl => {
-            const updatedImagesFolderUrl = [...prevImagesFolderUrl, ...newImagesFolderUrl];
-            form.setValue('imagesFolderUrl', updatedImagesFolderUrl);
-            return updatedImagesFolderUrl;
-        });
-    }, [form]);
+    const onDropChildFile = useCallback(
+        (acceptedFiles) => {
+            const file = acceptedFiles[0];
+            if (childFile) {
+                URL.revokeObjectURL(childFile.preview);
+            }
+            setChildFile(file);
+            form.setValue('childIdentificationInformationFile', file);
+            form.clearErrors('childIdentificationInformationFile');
+        },
+        [form, childFile],
+    );
+
+    const onDropImagesFolder = useCallback(
+        (acceptedFiles) => {
+            const newImagesFolderUrl = acceptedFiles.map((file) =>
+                Object.assign(file, {
+                    preview: URL.createObjectURL(file),
+                }),
+            );
+
+            setImagesFolderUrl((prevImagesFolderUrl) => {
+                const updatedImagesFolderUrl = [...prevImagesFolderUrl, ...newImagesFolderUrl];
+                form.setValue('imagesFolderUrl', updatedImagesFolderUrl);
+                return updatedImagesFolderUrl;
+            });
+        },
+        [form],
+    );
 
     const removeChildFile = () => {
         setChildFile(null);
         form.setValue('childIdentificationInformationFile', null);
     };
 
-
-
-
-    const onDropThumbnail = useCallback((acceptedFiles) => {
-        const file = acceptedFiles[0];
-        setThumbnail(Object.assign(file, {
-            preview: URL.createObjectURL(file)
-        }));
-        form.setValue('thumbnailUrl', file);
-        form.clearErrors('thumbnailUrl');
-    }, [form]);
+    const onDropThumbnail = useCallback(
+        (acceptedFiles) => {
+            const file = acceptedFiles[0];
+            setThumbnail(
+                Object.assign(file, {
+                    preview: URL.createObjectURL(file),
+                }),
+            );
+            form.setValue('thumbnailUrl', file);
+            form.clearErrors('thumbnailUrl');
+        },
+        [form],
+    );
 
     const removeImageFolder = (index) => {
         const newImagesFolderUrl = [...imagesFolderUrl];
@@ -255,35 +279,36 @@ const CampaignInfo = ({ }) => {
     const onSubmit = async (data) => {
         try {
             const targetAmount = parseFloat(data.targetAmount.replace(/,/g, ''));
-            const totalDisbursement = data.disbursementStages.reduce(
-                (sum, stage) => sum + stage.disbursementAmount,
-                0
-            );
+            const totalDisbursement = data.disbursementStages.reduce((sum, stage) => sum + stage.disbursementAmount, 0);
 
             if (totalDisbursement !== targetAmount) {
                 form.setError('disbursementStages', {
                     type: 'custom',
-                    message: `Tổng số tiền giải ngân (${totalDisbursement.toLocaleString()} VNĐ) phải bằng số tiền mục tiêu (${targetAmount.toLocaleString()} VNĐ)`
+                    message: `Tổng số tiền giải ngân (${totalDisbursement.toLocaleString()} VNĐ) phải bằng số tiền mục tiêu (${targetAmount.toLocaleString()} VNĐ)`,
                 });
                 toast.error('Vui lòng kiểm tra lại số tiền giải ngân');
                 return;
             }
+
             const userFolder = `user_${user.userID}`;
             const tempCampaignId = `c_${Date.now()}`; //timestamp
+
             // Upload thumbnail
             const thumbnailUrl = await uploadToCloudinary(
                 data.thumbnailUrl,
-                `${userFolder}/campaign/${tempCampaignId}`
+                `${userFolder}/campaign/${tempCampaignId}`,
             );
+
             // Upload imagesFolderUrl
             const imageUrls = await Promise.all(
-                data.imagesFolderUrl.map(file =>
-                    uploadToCloudinary(file, `${userFolder}/campaign/${tempCampaignId}/images-supported`)
-                )
+                data.imagesFolderUrl.map((file) =>
+                    uploadToCloudinary(file, `${userFolder}/campaign/${tempCampaignId}/images-supported`),
+                ),
             );
+
             const childIdentificationUrl = await uploadToCloudinary(
                 data.childIdentificationInformationFile,
-                `${userFolder}/campaign/${tempCampaignId}/child-identification`
+                `${userFolder}/campaign/${tempCampaignId}/child-identification`,
             );
 
             // Prepare the final data object
@@ -294,9 +319,9 @@ const CampaignInfo = ({ }) => {
                 childGender: data.childGender,
                 childLocation: data.childLocation,
                 childIdentificationInformationFile: childIdentificationUrl,
-                childWard: data.wardId,
-                childDistrict: data.districtId,
-                childProvince: data.provinceId,
+                childWard: selectedWard.name,
+                childDistrict: selectedDistrict.name,
+                childProvince: selectedProvince.name,
                 title: data.title,
                 story: data.story,
                 targetAmount: parseFloat(data.targetAmount.replace(/,/g, '')),
@@ -308,11 +333,11 @@ const CampaignInfo = ({ }) => {
                 imagesFolderUrl: imageUrls.join(','),
                 plannedStartDate: data.plannedStartDate.toISOString(),
                 plannedEndDate: data.plannedEndDate.toISOString(),
-                disbursementStages: data.disbursementStages.map(stage => ({
+                disbursementStages: data.disbursementStages.map((stage) => ({
                     disbursementAmount: stage.disbursementAmount,
                     scheduledDate: stage.scheduledDate.toISOString(),
-                    description: stage.description
-                }))
+                    description: stage.description,
+                })),
             };
 
             console.log('Final data to be sent to backend:', finalData);
@@ -324,7 +349,6 @@ const CampaignInfo = ({ }) => {
 
             toast.success('Chiến dịch được tạo thành công!');
             navigate('/guarantee/campaigns');
-
         } catch (error) {
             console.error('failed:', error);
             toast.error('Đã xảy ra lỗi! Vui lòng thử lại.');
@@ -333,13 +357,8 @@ const CampaignInfo = ({ }) => {
         }
     };
 
-    const formatNumber = (value) => {
-        return value.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    };
-
-
     return (
-        <div className='relative font-sans'>
+        <div className="relative font-sans">
             <Card className="w-full max-w-6xl mx-auto rounded-lg border-2">
                 <CardHeader>
                     <CardTitle className="text-center text-3xl">TẠO CHIẾN DỊCH</CardTitle>
@@ -379,22 +398,21 @@ const CampaignInfo = ({ }) => {
                             <div className="flex justify-center">
                                 <Button
                                     type="submit"
-                                    className={`w-1/2 ${isUploading || isCreatingCampaign ? 'bg-gray-400' : 'bg-[#2fabab]'} hover:bg-[#287176] text-white py-2 rounded-lg`}
+                                    className={`w-1/2 ${
+                                        isUploading || isCreatingCampaign ? 'bg-gray-400' : 'bg-[#2fabab]'
+                                    } hover:bg-[#287176] text-white py-2 rounded-lg`}
                                     disabled={isUploading || isCreatingCampaign}
                                 >
-                                    {(isUploading || isCreatingCampaign) ?
-                                        (<div className="flex items-center gap-2">
+                                    {isUploading || isCreatingCampaign ? (
+                                        <div className="flex items-center gap-2">
                                             <Loader2 className="animate-spin" size={18} />
                                             {isUploading ? 'Đang Tạo...' : 'Đang Tạo Hồ Sơ...'}
                                         </div>
-                                        ) : (
-                                            'Tạo Hồ Sơ'
-                                        )}
+                                    ) : (
+                                        'Tạo Hồ Sơ'
+                                    )}
                                 </Button>
                             </div>
-
-
-
                         </form>
                     </Form>
                 </CardContent>
